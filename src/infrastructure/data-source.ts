769fe37f15aad7c721bc6dfeb1e45f27e50bf710
Pablo Pessanha <pablopessanh@gmail.com>
2022-04-24 20:25:47 -0300

    refactor: renames config typeorm file

--- src/infrastructure/data-source.ts
+++ src/infrastructure/data-source.ts
@@ -1,16 +1,16 @@
-import { DataSource } from 'typeorm'
-import { User } from '@infrastructure/user/User'
+import { DataSource,  } from 'typeorm'
+import { User } from './user/User'
 
-export const connection = new DataSource({
+export const AppDataSource = new DataSource({
   type: 'postgres',
   host: 'localhost',
+  port: 5432,
   username: 'pgname',
   password: 'pgpassword',
-  database: 'Participation',
+  database: 'typeorm',
   synchronize: true,
   logging: true,
   entities: [User],
   migrations: ['src/infrastructure/migrations/*.ts'],
-  migrationsRun: true,
   migrationsTableName: 'MigrationsVersion'
 })
