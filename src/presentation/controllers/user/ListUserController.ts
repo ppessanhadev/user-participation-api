f23a8eff5575e286e9eed02fc42d53aa61a4cac5
Pablo Pessanha <pablopessanh@gmail.com>
2022-04-24 20:30:16 -0300

    feat: adds logic to recover users list on controller

--- src/presentation/controllers/user/ListUserController.ts
+++ src/presentation/controllers/user/ListUserController.ts
@@ -1,11 +1,15 @@
 import { Request, Response } from 'express'
 import { singleton } from 'tsyringe'
+import { connection } from '@infrastructure/data-source'
+import { User } from '@infrastructure/user/User'
 
 @singleton()
 export class ListUserController {
-  public handleRequest(_: Request, res: Response) {
+  public async handleRequest(_: Request, res: Response) {
     try {
-      return res.status(200).json(['users-list'])
+      const db = await connection.initialize()
+      const response = await db.manager.find(User)
+      return res.status(200).json(response)
     } catch (e) {
       const error = e as Error
       return res.status(400).json({ error })
