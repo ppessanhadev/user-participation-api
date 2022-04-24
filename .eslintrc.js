9d6c6ba1fd068c4552c80581f85ea33ab0f3076c
Pablo Pessanha <pablopessanh@gmail.com>
2022-04-24 15:48:26 -0300

    chore: update rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,6 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2020: true,
-    node: true,
-  },
-  extends: ['plugin:@typescript-eslint/recommended', 'airbnb-base', 'prettier'],
   parser: '@typescript-eslint/parser',
+  extends: ['plugin:@typescript-eslint/recommended', 'airbnb-base', 'prettier'],
   parserOptions: {
     ecmaVersion: 11,
     sourceType: 'module',
@@ -13,12 +8,14 @@ module.exports = {
   plugins: ['@typescript-eslint'],
   rules: {
     'no-use-before-define': 'off',
+    'no-unused-vars': 'off',
     'import/prefer-default-export': 'off',
     '@typescript-eslint/no-use-before-define': ['error'],
     'import/extensions': ['error', 'ignorePackages', { ts: 'never' }],
     'class-methods-use-this': ['off'],
     'arrow-body-style': ['off'],
     'no-restricted-syntax': ['off'],
+    'lines-between-class-members': ['off'],
     semi: [2, 'never'],
     quotes: [2, 'single', 'avoid-escape'],
     camelcase: ['off'],
